9b8ee0f70edcb44904af7fcbd4ffcbbe95002ede
Rahul Sabnis <rahulsabnis19@gmail.com>
2017-05-24 03:26:29 -0700

    Working music functionality

--- app/src/main/java/com/rahulsabnis/soundasleep/MainActivity.java
+++ app/src/main/java/com/rahulsabnis/soundasleep/MainActivity.java
@@ -1,20 +1,34 @@
 package com.rahulsabnis.soundasleep;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.support.v7.widget.AppCompatTextView;
 import android.util.Log;
+import android.view.View;
+import android.widget.NumberPicker;
+import android.widget.SeekBar;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
 import com.spotify.sdk.android.player.ConnectionStateCallback;
 import com.spotify.sdk.android.player.Error;
 import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
 
-public class MainActivity extends AppCompatActivity implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
+import mehdi.sakout.fancybuttons.FancyButton;
+
+public class MainActivity extends AppCompatActivity implements SpotifyPlayer.NotificationCallback,
+        ConnectionStateCallback {
 
     // TODO: Replace with your client ID
     private static final String CLIENT_ID = "71f0cb17348a4ab48954810749d84e09";
@@ -24,12 +38,19 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
     // Request code that will be used to verify if the result comes from correct activity
     private static final int REQUEST_CODE = 7971;
 
-    private Player mPlayer;
+    private static Player mPlayer;
+
+    private AppCompatTextView time;
+    private AppCompatSeekBar numberPicker;
+
+    private boolean spotifyLoaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spotifyLoaded = false;
 
         AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
                 AuthenticationResponse.Type.TOKEN,
@@ -38,15 +59,101 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
         AuthenticationRequest request = builder.build();
 
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
+        time = (AppCompatTextView)  findViewById(R.id.time);
+
+        numberPicker = (AppCompatSeekBar) findViewById(R.id.numPicker);
+        numberPicker.setMax(55);
+
+        time.setText((numberPicker.getProgress() + 5) + " min");
+
+        numberPicker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                StringBuilder build = new StringBuilder();
+                build.append((i + 5));
+                build.append(" min");
+                time.setText(build.toString());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+//        numberPicker.setMaxValue(60);
+//        numberPicker.setMinValue(5);
+
+        FancyButton fancy = (FancyButton) findViewById(R.id.pause);
+        fancy.setText("Start Music!");
+        fancy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mPlayer.playUri(new Player.OperationCallback() {
+                    @Override
+                    public void onSuccess() {
+                        mPlayer.setShuffle(null, true);
+                        mPlayer.skipToNext(null);
+                    }
+
+                    @Override
+                    public void onError(Error error) {
+                        AlertDialog dialog = new AlertDialog.Builder(MainActivity.this).setTitle("Error").
+                                setMessage("An error has occurred on playback. Please check your " +
+                                        "internet connectivity!").create();
+                        dialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        dialog.show();
+                    }
+                }, "spotify:user:allgaminglegend:playlist:7M2BTQqhtBsQ7Ru2ljl7W8", 0, 0);
+//                if (mPlayer.getPlaybackState().isPlaying) {
+//                    mPlayer.pause(null);
+//                } else {
+//                    mPlayer.resume(null);
+//                }
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+                    @Override
+                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
+                        mPlayer = spotifyPlayer;
+                        mPlayer.addConnectionStateCallback(MainActivity.this);
+                        mPlayer.addNotificationCallback(MainActivity.this);
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+        }
     }
 
     @Override
     protected void onDestroy() {
+        Spotify.destroyPlayer(this);
         super.onDestroy();
     }
 
@@ -73,6 +180,7 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
     @Override
     public void onLoggedIn() {
         Log.d("MainActivity", "User logged in");
+        spotifyLoaded = true;
     }
 
     @Override
@@ -93,5 +201,9 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
     @Override
     public void onConnectionMessage(String message) {
         Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+    public static Player getPlayer() {
+        return mPlayer;
     }
 }
